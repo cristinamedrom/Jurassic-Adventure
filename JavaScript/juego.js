914fabc1079b29b7eb015048b0ff3420b6022390
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-10 21:03:49 +0100

    imagen pajaro

--- JavaScript/juego.js
+++ JavaScript/juego.js
@@ -7,6 +7,7 @@ let suelo;
 let jumping = false;
 let rocaPrimen;
 let rocaSegun;
+let pajaroImg;
 
 function preload(){
 
@@ -16,6 +17,7 @@ function preload(){
     dinoMovil = loadImage('./img/caminar.gif');
     rocaPrimen = loadImage('./img/roca-primera.png');
     rocaSegun = loadImage('./img/roca-seg.png')
+    pajaroImg = loadImage('./img/pajaro.gif')
 
 }
 
@@ -24,6 +26,7 @@ function setup() {
     dino = new Player(168, 150, dinoQuieto, dinoMovil, 0.01, 0.1);
     rocaAlta = new RocaPrim(384, 164, rocaPrimen, 0.7, 0);
     rocaBaja = new RocaSegunda(512, 177, rocaSegun, 0.7, 0);
+    pajaritoFly = new Pajarito(525, 50, pajaroImg, 0.7, 0)
     suelo = new Suelo(0, 184, cesped, 0.7);
     
     createCanvas(384, 216);
@@ -47,10 +50,16 @@ function draw() {
     rocaBaja.draw();
     rocaBaja.update();
     if (rocaBaja.multpRoca2) {
-        rocaBaja = new RocaSegunda(512, 164, rocaSegun, 0.7, 0);
+        rocaBaja = new RocaSegunda(512, 177, rocaSegun, 0.7, 0);
         rocaBaja.multpRoca2 = false;
     }
     rocaBaja.choqueColosal2(dino);
+    pajaritoFly.draw();
+    pajaritoFly.update();
+    if (pajaritoFly.multpPajarito) {
+        pajaritoFly = new Pajarito(525, 50, pajaroImg, 0.7, 0);
+        pajaritoFly.multpPajarito = false;
+    }
 
 }
 
--- JavaScript/obstaculos.js
+++ JavaScript/obstaculos.js
@@ -82,13 +82,60 @@ class RocaSegunda {
         let dinoAlto = dino.y;
         let dinoBajo = dino.y + dino.dinoMovil.height;
 
-// dimensiones de RocaAlta
+// dimensiones de RocaBaja
         let rocaIzq2 = this.x;
         let rocaDcha2 = this.x + this.rocaSegun.width;
         let rocaAlto2 = this.y;
         let rocaBajo2 = this.y + this.rocaSegun.height;
 
         if(dinoIzq < rocaDcha2 && dinoDcha > rocaIzq2 && dinoAlto < rocaBajo2 && dinoBajo > rocaAlto2) {
+            enterPressed = false;
+            startRoca = false;
+            startSuelo = false;
+        }
+    }
+}
+
+class Pajarito {
+    constructor(x, y, pajaroImg, speed, grav) {
+        this.x = x;
+        this.y = y;
+        this.pajaroImg = pajaroImg;
+        this.speed = speed;
+        this.grav = grav;
+        this.multpPajarito = false;
+    }
+
+    update() {
+
+        if(startRoca) {
+            this.x -= this.speed;
+        }
+
+        if (this.x <= -this.pajaroImg.width) {
+            this.multpPajarito = true;
+        }
+
+    }
+
+    draw() {
+        image(pajaroImg, this.x, this.y);
+    }
+
+    choqueColosal3() {
+// dimensiones de Dino
+        let dinoIzq = dino.x;
+        let dinoDcha = dino.x + dino.dinoMovil.width;
+        let dinoAlto = dino.y;
+        let dinoBajo = dino.y + dino.dinoMovil.height;
+
+// dimensiones de Pajarito
+        let pajaritoIzq = this.x;
+        let pajaritoDcha = this.x + this.pajaroImg.width;
+        let pajaritoAlto = this.y;
+        let pajaritoBajo = this.y + this.pajaroImg.height;
+
+        if(dinoIzq < pajaritoDcha && dinoDcha > pajaritoIzq && dinoAlto < pajaritoBajo && dinoBajo > pajaritoAlto) {
             enterPressed = false;
             startRoca = false;
             startSuelo = false;
