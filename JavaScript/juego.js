16b22d9c63c13801fa45b6867d6001bf61fbaaa5
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-10 21:10:39 +0100

    mejorando el salto con la velocidad

--- JavaScript/juego.js
+++ JavaScript/juego.js
@@ -23,11 +23,11 @@ function preload(){
 
 function setup() {
     
-    dino = new Player(168, 150, dinoQuieto, dinoMovil, 0.01, 0.1);
-    rocaAlta = new RocaPrim(384, 164, rocaPrimen, 0.7, 0);
-    rocaBaja = new RocaSegunda(512, 177, rocaSegun, 0.7, 0);
-    pajaritoFly = new Pajarito(525, 50, pajaroImg, 0.7, 0)
-    suelo = new Suelo(0, 184, cesped, 0.7);
+    dino = new Player(168, 150, dinoQuieto, dinoMovil, -11, 1);
+    rocaAlta = new RocaPrim(384, 164, rocaPrimen, 3, 0);
+    rocaBaja = new RocaSegunda(512, 177, rocaSegun, 3, 0);
+    pajaritoFly = new Pajarito(525, 50, pajaroImg, 8, 0)
+    suelo = new Suelo(0, 184, cesped, 3);
     
     createCanvas(384, 216);
 
@@ -43,21 +43,21 @@ function draw() {
     rocaAlta.draw();
     rocaAlta.update();
     if (rocaAlta.multpRoca) {
-        rocaAlta = new RocaPrim(384, 164, rocaPrimen, 0.7, 0);
+        rocaAlta = new RocaPrim(384, 164, rocaPrimen, 3, 0);
         rocaAlta.multpRoca = false;
     }
     rocaAlta.choqueColosal(dino);
     rocaBaja.draw();
     rocaBaja.update();
     if (rocaBaja.multpRoca2) {
-        rocaBaja = new RocaSegunda(512, 177, rocaSegun, 0.7, 0);
+        rocaBaja = new RocaSegunda(512, 177, rocaSegun, 3, 0);
         rocaBaja.multpRoca2 = false;
     }
     rocaBaja.choqueColosal2(dino);
     pajaritoFly.draw();
     pajaritoFly.update();
     if (pajaritoFly.multpPajarito) {
-        pajaritoFly = new Pajarito(525, 50, pajaroImg, 0.7, 0);
+        pajaritoFly = new Pajarito(525, 50, pajaroImg, 8, 0);
         pajaritoFly.multpPajarito = false;
     }
 
